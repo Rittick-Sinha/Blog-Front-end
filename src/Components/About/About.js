b9e0d01e3552f57c7775b33bd407ab4d798e58cb
Rittick-Sinha <111774844+Rittick-Sinha@users.noreply.github.com>
2024-03-06 00:54:45 +0530

    about page created

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -50,6 +50,7 @@ const About = () => {
                         </div>
                     </div>
                 </div>
+                
                 <MovingCard />
             </div>
             <Footer />
--- src/Components/Moving_card/Moving_card.js
+++ src/Components/Moving_card/Moving_card.js
@@ -2,37 +2,130 @@ import React from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import './Style.css';
+import profile from "../assests/profile.jpg"
 const MovingCard = () => {
     const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
         desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
+            breakpoint: {
+                max: 3000,
+                min: 1024
+            },
+            items: 3,
+            partialVisibilityGutter: 40
         },
         mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
+            breakpoint: {
+                max: 464,
+                min: 0
+            },
+            items: 1,
+            partialVisibilityGutter: 30
+        },
+        tablet: {
+            breakpoint: {
+                max: 1024,
+                min: 464
+            },
+            items: 2,
+            partialVisibilityGutter: 30
         }
     };
     return (
         <>
             <div className="moving-card">
-                <Carousel responsive={responsive}>
-                    <div className='card'>
-                        
+                <Carousel
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlay
+                    autoPlaySpeed={1500}
+                    centerMode={false}
+                    className=""
+                    containerClass="container-with-dots"
+                    dotListClass=""
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    itemClass=""
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    pauseOnHover
+                    renderArrowsWhenDisabled={false}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={responsive}
+                    rewind={false}
+                    rewindWithAnimation={false}
+                    rtl={false}
+                    shouldResetAutoplay={false}
+                    showDots={false}
+                    sliderClass=""
+                    slidesToSlide={1}
+                    swipeable>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
                     </div>
-                    <div>Item 2</div>
-                    <div>Item 3</div>
-                    <div>Item 4</div>
-                </Carousel>;
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src={profile} alt="" />
+                        </div>
+                        <span>"Wow, your blog is incredibly insightful and well-written! I always look forward to reading your posts for inspiration and knowledge."</span>
+                        <div className="stroke"></div>
+                        <span1>Rittick Sinha</span1>
+                    </div>
+                </Carousel>
             </div>
         </>
     );
