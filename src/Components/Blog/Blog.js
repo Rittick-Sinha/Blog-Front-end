774e2fe0143de3e57575350d41baa96ed76f28aa
Rittick-Sinha <111774844+Rittick-Sinha@users.noreply.github.com>
2024-03-07 23:54:45 +0530

    cards sdded in the blog page

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import BlogCard from '../BlogCard/BlogCard'
+import './blog.css';
 const Blog = () => {
     return (
         <>
             <Navbar />
-            Blog
+            <div className="blog-body">
+                <div className="boxx1">
+                    <BlogCard />
+                </div>
+                <div className="boxx2">
+                    
+                </div>
+            </div>
             <Footer />
         </>
     );
---
+++ src/Components/BlogCard/BlogCard.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './blogcard.css';
+import mind from '../assests/mind.jpg';
+const BlogCard = (props) => {
+    return (
+        <>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-card">
+                <div className="sub-card">
+                    <div className="img">
+                        <img src={mind} alt="" />
+                    </div>
+                    <div className="content">
+                        <span>The Power of Mindfulness: Nurturing Mental Well-being</span>
+                        <span2>December 27, 2023</span2>
+                        <p>Discover the transformative benefits of mindfulness practices and cultivate inner peace in a fast-paced world.</p>
+                        <a href="/#"><span>read more</span></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default BlogCard;
