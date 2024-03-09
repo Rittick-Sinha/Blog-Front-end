e0fa42a130989e1eb8e75cb14b0a6d5606a8d60d
Rittick-Sinha <111774844+Rittick-Sinha@users.noreply.github.com>
2024-03-09 22:57:16 +0530

    added serach box in the blog page

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import BlogCard from '../BlogCard/BlogCard'
+import SearchButton from '../Search button/Search';
 import './blog.css';
 const Blog = () => {
     return (
@@ -12,7 +13,16 @@ const Blog = () => {
                     <BlogCard />
                 </div>
                 <div className="boxx2">
-                    
+                    <SearchButton />
+                    <div className="categories">
+                        <ul>
+                            <li>Food</li>
+                            <li>Technology</li>
+                            <li>Travel</li>
+                            <li>Food</li>
+                            <li>Tourism</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <Footer />
