b80c035f4f3b626fe230b0f10ce7d6de05cb97ea
Rittick-Sinha <111774844+Rittick-Sinha@users.noreply.github.com>
2024-03-10 15:25:19 +0530

    test2

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -14,51 +14,6 @@ const Blog = () => {
                 <div className="boxx1">
                     <BlogCard />
                 </div>
-                <div className="boxx2">
-                    <div className="content">
-                        <SearchButton />
-                        <div className="categories">
-                            <span>Categories</span>
-                            <span1>Food</span1>
-                            <span1>Technology</span1>
-                            <span1>Travel</span1>
-                            <span1>Food</span1>
-                            <span1>Tourism</span1>
-                        </div>
-                        <div className="recent-post">
-                            <div className="header">
-                                RECENT POSTS
-                            </div>
-                            <div className="card">
-                                <div className="img">
-                                    <img src={img2} alt="" />
-                                </div>
-                                <div className="content">
-                                    <span>Culinary Crossroads: Exploring the Fusion of Flavors</span>
-                                    <span2>Feburary 12, 2024</span2>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="img">
-                                    <img src={img1} alt="" />
-                                </div>
-                                <div className="content">
-                                    <span> Wanderlust Chronicles: Unveiling the Hidden Gems of Southeast Asia</span>
-                                    <span2>Feburary 10, 2024</span2>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <div className="img">
-                                    <img src={img1} alt="" />
-                                </div>
-                                <div className="content">
-                                    <span> Wanderlust Chronicles: Unveiling the Hidden Gems of Southeast Asia</span>
-                                    <span2>Feburary 10, 2024</span2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
             <Footer />
         </>
